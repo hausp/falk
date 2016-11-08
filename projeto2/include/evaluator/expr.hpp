a732aab2db423b1b1b5af061292927f51213df63
aszdrick <aszdrick@gmail.com>
2016-11-07 23:02:28 -0200

    missing files added

---
+++ projeto2/include/evaluator/expr.hpp
@@ -0,0 +1,27 @@
+
+// #ifndef FALK_EVALUATOR_ARITHMETIC_EXPR_HPP
+// #define FALK_EVALUATOR_ARITHMETIC_EXPR_HPP
+
+// #include "base/operators.hpp"
+// #include "Evaluator.hpp"
+// #include "rvalue.hpp"
+
+// namespace falk {
+//     namespace ev {
+//         template<typename Type>
+//         class expr : public ast::tdnode<2, Evaluator> {
+//             friend class Evaluator;
+//          public:
+//             expr(Type, Evaluator::Type,
+//                 ast::tnode<Evaluator>, ast::tnode<Evaluator>);
+//          private:
+//             Type operation;
+//         };
+
+//         expr<op::Arithmetical> operator+(const rvalue& lhs, const rvalue& rhs) {
+
+//         }
+//     }
+// }
+
+// #endif /* FALK_EVALUATOR_ARITHMETIC_EXPR_HPP */
---
+++ projeto2/include/evaluator/numeric.hpp
@@ -0,0 +1,68 @@
+
+#ifndef FALK_EVALUATOR_RVALUE
+#define FALK_EVALUATOR_RVALUE
+
+#include <ostream>
+#include "base/types.hpp"
+
+namespace falk {
+    namespace ev {
+        struct numeric {
+            Type type;
+            double real;
+            double imag = 0;
+        };
+
+        inline numeric operator+(const numeric& lhs, const numeric& rhs) {
+            auto type = falk::resolve_types(lhs.type, rhs.type);
+            if (type == Type::COMPLEX) {
+                return {type, lhs.real + rhs.real, lhs.imag + rhs.imag};
+            } else {
+                return {type, lhs.real + rhs.real};
+            }
+        }
+
+        inline numeric operator-(const numeric& lhs, const numeric& rhs) {
+            auto type = falk::resolve_types(lhs.type, rhs.type);
+            if (type == Type::COMPLEX) {
+                return {type, lhs.real - rhs.real, lhs.imag - rhs.imag};
+            } else {
+                return {type, lhs.real - rhs.real};
+            }
+        }
+
+        inline numeric operator-(const numeric& n) {
+            return {n.type, -n.real, -n.imag};
+        }
+
+        inline numeric operator*(const numeric& lhs, const numeric& rhs) {
+            auto type = falk::resolve_types(lhs.type, rhs.type);
+            if (type == Type::COMPLEX) {
+
+            } else {
+                return {type, lhs.real * rhs.real};
+            }
+        }
+
+        inline numeric operator/(const numeric& lhs, const numeric& rhs) {
+            auto type = falk::resolve_types(lhs.type, rhs.type);
+            if (type == Type::COMPLEX) {
+
+            } else {
+                return {type, lhs.real / rhs.real};
+            }
+        }
+
+        inline std::ostream& operator<<(std::ostream& out, const numeric& n) {
+            if (n.type == Type::COMPLEX) {
+                // TODO: improve this
+                out << n.real << " + " << n.imag << "i";
+            } else {
+                out << n.real;
+            }
+            return out;
+        }
+    }
+}
+
+#endif /* FALK_EVALUATOR_RVALUE */
