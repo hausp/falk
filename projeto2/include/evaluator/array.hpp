4f1303d387cafbf2bb81b74da2babd923d872058
viniciusmctf <ghabriel.nunes@gmail.com>
2016-11-21 16:57:27 -0200

    Implemented the operators of array and placeholders to matrix

--- projeto2/include/evaluator/array.hpp
+++ projeto2/include/evaluator/array.hpp
@@ -2,26 +2,148 @@
 #ifndef FALK_EV_ARRAY_HPP
 #define FALK_EV_ARRAY_HPP
 
+#include <vector>
+#include "scalar.hpp"
+
 namespace falk {
     namespace ev {
-        struct array {
-            array pow(const array&);
-            array& pow_assign(const array&);
+        class array {
+         public:
+            size_t size() const {
+                return values.size();
+            }
 
-            array& operator+=(const array&);
-            array& operator-=(const array&);
-            array& operator*=(const array&);
-            array& operator/=(const array&);
-            array& operator%=(const array&);
+            scalar& operator[](size_t index) {
+                return values[index];
+            }
+
+            void push_back(const scalar& value) {
+                values.push_back(value);
+            }
+
+            const scalar& operator[](size_t index) const {
+                return values.at(index);
+            }
+
+            array pow(const array& rhs) {
+                if (size() != rhs.size()) {
+                    // TODO: error (incompatible operands)
+                    return array{};
+                }
+
+                array result;
+                for (size_t i = 0; i < size(); i++) {
+                    result.push_back(values[i].pow(rhs[i]));
+                }
+                return result;
+            }
+
+            array& pow_assign(const array& rhs) {
+                return *this = pow(rhs);
+            }
+
+            array operator+=(const array& rhs) {
+                if (size() != rhs.size()) {
+                    // TODO: error (incompatible operands)
+                    return array{};
+                }
+
+                array result;
+                for (size_t i = 0; i < size(); i++) {
+                    result.push_back(values[i] + rhs[i]);
+                }
+                return result;
+            }
+
+            array operator-=(const array& rhs) {
+                if (size() != rhs.size()) {
+                    // TODO: error (incompatible operands)
+                    return array{};
+                }
+
+                array result;
+                for (size_t i = 0; i < size(); i++) {
+                    result.push_back(values[i] - rhs[i]);
+                }
+                return result;
+            }
+
+            array operator*=(const array& rhs) {
+                if (size() != rhs.size()) {
+                    // TODO: error (incompatible operands)
+                    return array{};
+                }
+
+                array result;
+                for (size_t i = 0; i < size(); i++) {
+                    result.push_back(values[i] * rhs[i]);
+                }
+                return result;
+            }
+
+            array operator/=(const array& rhs) {
+                if (size() != rhs.size()) {
+                    // TODO: error (incompatible operands)
+                    return array{};
+                }
+
+                array result;
+                for (size_t i = 0; i < size(); i++) {
+                    result.push_back(values[i] / rhs[i]);
+                }
+                return result;
+            }
+
+            array operator%=(const array& rhs) {
+                if (size() != rhs.size()) {
+                    // TODO: error (incompatible operands)
+                    return array{};
+                }
+
+                array result;
+                for (size_t i = 0; i < size(); i++) {
+                    result.push_back(values[i] % rhs[i]);                
+                }
+                return result;
+            }
+
+         private:
+            std::vector<scalar> values;
         };
 
-        array operator+(const array&, const array&);
-        array operator-(const array&, const array&);
-        array operator*(const array&, const array&);
-        array operator/(const array&, const array&);
-        array operator%(const array&, const array&);
-        array operator-(const array&);
 
+        inline array operator+(const array& lhs, const array& rhs) {
+            auto copy = lhs;
+            return copy += rhs;
+        }
+
+        inline array operator-(const array& lhs, const array& rhs) {
+            auto copy = lhs;
+            return copy -= rhs;
+        }
+
+        inline array operator*(const array& lhs, const array& rhs) {
+            auto copy = lhs;
+            return copy *= rhs;
+        }
+
+        inline array operator/(const array& lhs, const array& rhs) {
+            auto copy = lhs;
+            return copy /= rhs;
+        }
+
+        inline array operator%(const array& lhs, const array& rhs) {
+            auto copy = lhs;
+            return copy %= rhs;
+        }
+
+        inline array operator-(const array& rhs) {
+            array result;
+            for (size_t i = 0; i < rhs.size(); i++) {
+                result.push_back(-rhs[i]);
+            }
+            return result;
+        }
     }
 }
 
--- projeto2/include/evaluator/matrix.hpp
+++ projeto2/include/evaluator/matrix.hpp
@@ -2,26 +2,151 @@
 #ifndef FALK_EV_MATRIX_HPP
 #define FALK_EV_MATRIX_HPP
 
+#include <vector>
+#include "scalar.hpp"
+
+// Assigned to: Merlseson
 namespace falk {
     namespace ev {
-        struct matrix {
-            matrix pow(const matrix&);
-            matrix& pow_assign(const matrix&);
+        class matrix {
+         public:
+            size_t size() const {
+                return values.size();
+            }
 
-            matrix& operator+=(const matrix&);
-            matrix& operator-=(const matrix&);
-            matrix& operator*=(const matrix&);
-            matrix& operator/=(const matrix&);
-            matrix& operator%=(const matrix&);
+            scalar& operator[](size_t index) {
+                return values[index];
+            }
+
+            void push_back(const scalar& value) {
+                values.push_back(value);
+            }
+
+            const scalar& operator[](size_t index) const {
+                return values.at(index);
+            }
+
+            matrix pow(const matrix& rhs) {
+                if (size() != rhs.size()) {
+                    // TODO: error (incompatible operands)
+                    return matrix{};
+                }
+
+                matrix result;
+                for (size_t i = 0; i < size(); i++) {
+                    result.push_back(values[i].pow(rhs[i]));
+                }
+                return result;
+            }
+
+            matrix& pow_assign(const matrix& rhs) {
+                return *this = pow(rhs);
+            }
+
+            matrix operator+=(const matrix& rhs) {
+                if (size() != rhs.size()) {
+                    // TODO: error (incompatible operands)
+                    return matrix{};
+                }
+
+                matrix result;
+                for (size_t i = 0; i < size(); i++) {
+                    result.push_back(values[i] + rhs[i]);
+                }
+                return result;
+            }
+
+            matrix operator-=(const matrix& rhs) {
+                if (size() != rhs.size()) {
+                    // TODO: error (incompatible operands)
+                    return matrix{};
+                }
+
+                matrix result;
+                for (size_t i = 0; i < size(); i++) {
+                    result.push_back(values[i] - rhs[i]);
+                }
+                return result;
+            }
+
+            matrix operator*=(const matrix& rhs) {
+                if (size() != rhs.size()) {
+                    // TODO: error (incompatible operands)
+                    return matrix{};
+                }
+
+                matrix result;
+                for (size_t i = 0; i < size(); i++) {
+                    result.push_back(values[i] * rhs[i]);
+                }
+                return result;
+            }
+
+            matrix operator/=(const matrix& rhs) {
+                if (size() != rhs.size()) {
+                    // TODO: error (incompatible operands)
+                    return matrix{};
+                }
+
+                matrix result;
+                for (size_t i = 0; i < size(); i++) {
+                    result.push_back(values[i] / rhs[i]);
+                }
+                return result;
+            }
+
+            matrix operator%=(const matrix& rhs) {
+                if (size() != rhs.size()) {
+                    // TODO: error (incompatible operands)
+                    return matrix{};
+                }
+
+                matrix result;
+                for (size_t i = 0; i < size(); i++) {
+                    result.push_back(values[i] % rhs[i]);                
+                }
+                return result;
+            }
+
+         private:
+            std::vector<scalar> values;
+            size_t num_rows;
+            size_t num_columns;
         };
 
-        matrix operator+(const matrix&, const matrix&);
-        matrix operator-(const matrix&, const matrix&);
-        matrix operator*(const matrix&, const matrix&);
-        matrix operator/(const matrix&, const matrix&);
-        matrix operator%(const matrix&, const matrix&);
-        matrix operator-(const matrix&);
 
+        inline matrix operator+(const matrix& lhs, const matrix& rhs) {
+            auto copy = lhs;
+            return copy += rhs;
+        }
+
+        inline matrix operator-(const matrix& lhs, const matrix& rhs) {
+            auto copy = lhs;
+            return copy -= rhs;
+        }
+
+        inline matrix operator*(const matrix& lhs, const matrix& rhs) {
+            auto copy = lhs;
+            return copy *= rhs;
+        }
+
+        inline matrix operator/(const matrix& lhs, const matrix& rhs) {
+            auto copy = lhs;
+            return copy /= rhs;
+        }
+
+        inline matrix operator%(const matrix& lhs, const matrix& rhs) {
+            auto copy = lhs;
+            return copy %= rhs;
+        }
+
+        inline matrix operator-(const matrix& rhs) {
+            matrix result;
+            for (size_t i = 0; i < rhs.size(); i++) {
+                result.push_back(-rhs[i]);
+            }
+            return result;
+        }
     }
 }
 
