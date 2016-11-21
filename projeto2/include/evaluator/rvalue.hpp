c38bf0e7289eed9ae80955a86ade61425d9d2c1c
aszdrick <aszdrick@gmail.com>
2016-11-21 02:17:12 -0200

    rvalue cleanup

--- projeto2/include/evaluator/rvalue.hpp
+++ projeto2/include/evaluator/rvalue.hpp
@@ -2,170 +2,89 @@
 #ifndef FALK_EVALUATOR_RVALUE
 #define FALK_EVALUATOR_RVALUE
 
-#include <ostream>
+#include <complex>
 #include "base/types.hpp"
 
 namespace falk {
     namespace ev {
-        struct rvalue {
-            rvalue(double v):
-              type{falk::type::REAL}, real{v} { }
+        // Class to capture all kinds of rvalue possible in
+        // interpreted mode.
+        class rvalue {
+         public:
+            rvalue(double);
+            rvalue(std::complex<double>);
+            rvalue(falk::type = falk::type::UNDEFINED, double = 0, double = 0);
+            rvalue(bool);
+
+            bool boolean() const;
+            std::complex<double> complex() const;
             
-            rvalue(std::complex<double> v):
-              type{falk::type::COMPLEX}, real{v.real()}, imag{v.imag()} { }
-            
-            rvalue(falk::type type = falk::type::UNDEFINED, double real = 0, double imag = 0):
-              type{type}, real{real}, imag{imag} { }
-            
-            rvalue(bool v):
-              type{falk::type::BOOL}, real{static_cast<double>(v)} { }
+            double imag() const;
+            double real() const;
+            falk::type type() const;
 
-            std::complex<double> complex() const { return {real, imag}; }
+            operator std::complex<double>() const;
+            operator long() const;
 
-            bool boolean() const { return static_cast<bool>(real); }
-            
-            operator std::complex<double>() const { return {real, imag}; }
-
-            operator long() const { return static_cast<long>(real); }
-
-            inline rvalue pow(const rvalue& rhs) {
-                auto result_type = falk::resolve_types(type, rhs.type);
-                switch (result_type) {
-                    case falk::type::COMPLEX:
-                        return std::pow(complex(), rhs);
-                    case falk::type::REAL:
-                        return {result_type, std::pow(real, rhs.real)};
-                    case falk::type::BOOL:
-                        // TODO: error
-                        return {};
-                    default:
-                        // TODO: throw a brick at the user
-                        return {};
-                }
-            }
-
-            falk::type type;
-            double real;
-            double imag = 0;
+            rvalue pow(const rvalue&);
+         private:
+            falk::type _type;
+            double _real;
+            double _imag = 0;
         };
 
-        inline rvalue operator+(const rvalue& lhs, const rvalue& rhs) {
-            auto type = falk::resolve_types(lhs.type, rhs.type);
-            switch (type) {
-                case falk::type::COMPLEX:
-                    return {type, lhs.real + rhs.real, lhs.imag + rhs.imag};
-                case falk::type::REAL:
-                    return {type, lhs.real + rhs.real};
-                case falk::type::BOOL:
-                    // TODO: error
-                    return {};
-                default:
-                    // TODO: throw a brick at the user
-                    return {};
-            }
+        // Non-member functions
+        rvalue operator+(const rvalue&, const rvalue&);
+        rvalue operator-(const rvalue&, const rvalue&);
+        rvalue operator*(const rvalue&, const rvalue&);
+        rvalue operator/(const rvalue&, const rvalue&);
+        rvalue operator%(const rvalue&, const rvalue&);
+        rvalue operator-(const rvalue&);
+
+        std::ostream& operator<<(std::ostream&, const rvalue&);
+
+        // inline implementations
+
+        inline rvalue::rvalue(double v):
+          _type{falk::type::REAL}, _real{v} { }
+        
+        inline rvalue::rvalue(std::complex<double> v):
+          _type{falk::type::COMPLEX}, _real{v.real()}, _imag{v.imag()} { }
+        
+        inline rvalue::rvalue(falk::type type, double real, double imag):
+          _type{type}, _real{real}, _imag{imag} { }
+        
+        inline rvalue::rvalue(bool v):
+          _type{falk::type::BOOL}, _real{static_cast<double>(v)} { }
+
+        inline std::complex<double> rvalue::complex() const {
+            return {_real, _imag};
         }
 
-        inline rvalue operator-(const rvalue& lhs, const rvalue& rhs) {
-            auto type = falk::resolve_types(lhs.type, rhs.type);
-            switch (type) {
-                case falk::type::COMPLEX:
-                    return {type, lhs.real - rhs.real, lhs.imag - rhs.imag};
-                case falk::type::REAL:
-                    return {type, lhs.real - rhs.real};
-                case falk::type::BOOL:
-                    // TODO: error
-                    return {};
-                default:
-                    // TODO: throw a brick at the user
-                    return {};
-            }
+        inline bool rvalue::boolean() const {
+            return static_cast<bool>(_real);
         }
 
-        inline rvalue operator-(const rvalue& n) {
-            auto type = n.type;
-            switch (type) {
-                case falk::type::COMPLEX:
-                case falk::type::REAL:
-                    return {n.type, -n.real, -n.imag};
-                case falk::type::BOOL:
-                    // TODO: error
-                    return {};
-                default:
-                    // TODO: throw a brick at the user
-                    return {};
-            }
+        inline double rvalue::imag() const {
+            return _imag;
         }
 
-        inline rvalue operator*(const rvalue& lhs, const rvalue& rhs) {
-            auto type = falk::resolve_types(lhs.type, rhs.type);
-            switch (type) {
-                case falk::type::COMPLEX:
-                    return lhs.complex() * rhs.complex();
-                case falk::type::REAL:
-                    return lhs.real * rhs.real;
-                case falk::type::BOOL:
-                    // TODO: error
-                    return {};
-                default:
-                    // TODO: throw a brick at the user
-                    return {};
-            }
+        inline double rvalue::real() const {
+            return _real;
         }
 
-        inline rvalue operator/(const rvalue& lhs, const rvalue& rhs) {
-            auto type = falk::resolve_types(lhs.type, rhs.type);
-            switch (type) {
-                case falk::type::COMPLEX:
-                    return lhs / rhs;
-                case falk::type::REAL:
-                    return lhs.real / rhs.real;
-                case falk::type::BOOL:
-                    // TODO: throw a error
-                    return {};
-                default:
-                    // TODO: throw a brick at the user
-                    return {};
-            }
+        inline falk::type rvalue::type() const {
+            return _type;
+        }
+        
+        inline rvalue::operator std::complex<double>() const {
+            return {_real, _imag};
         }
 
-        inline rvalue operator%(const rvalue& lhs, const rvalue& rhs) {
-            auto type = falk::resolve_types(lhs.type, rhs.type);
-            switch (type) {
-                case falk::type::COMPLEX:
-                    // TODO
-                    return {};
-                case falk::type::REAL:
-                    return std::fmod(lhs.real, rhs.real);
-                case falk::type::BOOL:
-                    // TODO: throw a error
-                    return {};
-                default:
-                    // TODO: throw a brick at the user
-                    return {};
-            }
+        inline rvalue::operator long() const {
+            return static_cast<long>(_real);
         }
 
-
-        inline std::ostream& operator<<(std::ostream& out, const rvalue& n) {
-            auto type = n.type;
-            switch (type) {
-                case falk::type::COMPLEX:
-                    if (std::signbit(n.imag)) {
-                        out << n.real << " - " << std::abs(n.imag) << "i";
-                    } else {
-                        out << n.real << " + " << n.imag << "i";
-                    }
-                    break;
-                case falk::type::REAL:
-                    out << n.real;
-                    break;
-                case falk::type::BOOL:
-                    out << std::boolalpha << n << std::noboolalpha;
-                    break;
-                default:;
-            }
-            return out;
-        }
     }
 }
 
